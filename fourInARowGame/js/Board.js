6d5421390bd1a2e062199e77b0b8569cbc52ec68
Natalia Kozoriz <webkng@gmail.com>
2018-12-02 18:27:09 +0200

    added left, right, down methods

--- fourInARowGame/js/Board.js
+++ fourInARowGame/js/Board.js
@@ -1,31 +1,33 @@
-class Board{
-    constructor(){
-        this.cols = 6;
-        this.rows = 7;
+class Board {
+    constructor() {
+        this.rows = 6;
+        this.columns = 7;
         this.spaces = this.createSpaces();
     }
-    createSpaces(){
+    
+  
+    createSpaces() {
         const spaces = [];
-
-        for (let x = 0; x < this.cols; x++){
-            const column = [];
-            for (let y = 0; y < this.rows; y++ ){
-                const space = new Space (x, y);
-                column.push(space);
-            }
-            spaces.push(column);
-        }
+        
+		for (let x = 0; x < this.columns; x++) {
+			const col = [];
+			
+			for (let y = 0; y < this.rows; y++) {
+				const space = new Space(x, y);
+				col.push(space);
+			}
+			
+			spaces.push(col);
+		}
+        
         return spaces;
     }
-
-    drawHTMLBoard(){
-
-        for (let column of this.spaces){
-            for (let space of column){
+ 
+	drawHTMLBoard() {
+        for (let column of this.spaces) {
+            for (let space of column) {
                 space.drawSVGSpace();
             }
         }
-    }
-
-
+	} 
 }
